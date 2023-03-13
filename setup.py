fc05ba5c6bbde666a8aba7d936b056a655e84473
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-03-13 11:08:40 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def requirements():
 
 setuptools.setup(
 	name='python-supporter',
-	version='0.0.5',
+	version='0.0.6',
 	description='Python supporter',
 	long_description=open('README.md').read(),
 	long_description_content_type='text/markdown',
