9bc1a63b87593ce51301a9b2622b49a9c706b64c
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-03-18 01:17:48 +0900

    Update config.py

--- python_supporter/config.py
+++ python_supporter/config.py
@@ -1,3 +1,4 @@
+'''
 import json
 import os
 
@@ -20,4 +21,29 @@ def save_config_text(file, text):
     #with open(file, "w", encoding='utf8') as f:
     #    f.write(text)
     d = json.loads(text)
+    save_config_dict(file, d)
+'''
+
+import json
+import os
+
+def load_config_from_json_file(json_file):
+    if os.path.exists(json_file):
+        with open(json_file, encoding="utf8") as f:
+            config = json.load(f)
+        return config
+    return None
+
+def load_config_from_json_text(json_text):
+    config = json.loads(json_text)
+    return config
+
+def save_config(file, config):
+    with open(file, "w", encoding='utf8') as f:
+        json.dump(config, f, indent=4)
+
+def save_json_text(file, json_text):
+    #with open(file, "w", encoding='utf8') as f:
+    #    f.write(json_text)
+    d = json.loads(json_text)
     save_config_dict(file, d)
