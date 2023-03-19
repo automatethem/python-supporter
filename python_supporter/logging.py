b0dc48f442e7192698b5ef7b03b36aab1a7be929
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-03-19 22:24:40 +0900

    Update logging.py

--- python_supporter/logging.py
+++ python_supporter/logging.py
@@ -1,3 +1,4 @@
+'''
 import logging
 import sys
 
@@ -5,12 +6,10 @@ DEBUG = logging.DEBUG
 INFO = logging.INFO
 ERROR = logging.ERROR
 
-'''
-level = python_supporter.logging.DEBUG
-#level = python_supporter.logging.INFO
-#level = python_supporter.logging.ERROR
-python_supporter.logging.basic_config(level)
-'''
+#level = python_supporter.logging.DEBUG
+##level = python_supporter.logging.INFO
+##level = python_supporter.logging.ERROR
+#python_supporter.logging.basic_config(level)
 def basic_config(level=DEBUG, log_file="log.txt"):
     for name in logging.root.manager.loggerDict:
         logger = logging.getLogger(name)
@@ -26,6 +25,20 @@ def basic_config(level=DEBUG, log_file="log.txt"):
         datefmt = '%Y-%m-%d %H:%M:%S',
         level = level
     )
+
+def debug(message):
+    logging.debug(message)
+
+def info(message):
+    logging.info(message)
+
+def error(message):
+    logging.error(message)
+'''
+import logging
+
+def basicConfig(level, format='%(asctime)s: %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S', filename=None):
+    logging.basicConfig(level=level, format=format, datefmt=datefmt, filename=filename)
 
 def debug(message):
     logging.debug(message)
