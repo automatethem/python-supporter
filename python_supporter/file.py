95ca3d8a1a726390749f9994d2c5369dac4e5dea
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-05-02 16:51:13 +0900

    Update file.py

--- python_supporter/file.py
+++ python_supporter/file.py
@@ -1,6 +1,9 @@
 def read_file(file):
-    with open(file, encoding="utf8") as f:
-        text = f.read()
+    try:
+        with open(file, encoding="utf8") as f:
+            text = f.read()
+    except:
+        text = ""
     return text
 
 def write_file(file, text):
