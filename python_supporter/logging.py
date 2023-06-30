fe99d377c9bffc591edbf18b7591a228d101f0c7
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-06-30 15:45:02 +0900

    Update logging.py

--- python_supporter/logging.py
+++ python_supporter/logging.py
@@ -4,7 +4,7 @@ DEBUG = logging.DEBUG
 INFO = logging.INFO
 ERROR = logging.ERROR
 
-def basicConfig(level, format='%(asctime)s: %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S', filename=None):
+def basicConfig(level=DEBUG, format='%(asctime)s: %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S', filename=None):
     logging.basicConfig(level=level, format=format, datefmt=datefmt, filename=filename)
 
 def debug(message):
