c6cb4fa02fc55a33d5e17974933657a5c2ad9372
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-07-06 15:59:52 +0900

    Update check_ip.py

--- python_supporter/check_ip.py
+++ python_supporter/check_ip.py
@@ -5,7 +5,7 @@ import requests
 def check_ip():    
     try:
         response = requests.get("https://api.myip.com")
-        print(response.json()) #{"ip":"180.92.96.163","country":"Korea, Republic of","cc":"KR"}
+        #print(response.json()) #{"ip":"180.92.96.163","country":"Korea, Republic of","cc":"KR"}
         return response.json()["ip"]
     except:
         return None
