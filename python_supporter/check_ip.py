9aec4cf39a2c9297de929f16edac1f199606f5f0
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-07-12 16:11:16 +0900

    Update check_ip.py

--- python_supporter/check_ip.py
+++ python_supporter/check_ip.py
@@ -1,5 +1,6 @@
 import requests
 
+'''
 #https://www.myip.com/api-docs/
 #https://api.myip.com/
 def check_ip():    
@@ -9,3 +10,14 @@ def check_ip():
         return response.json()["ip"]
     except:
         return None
+'''
+#'''
+#https://api64.ipify.org/?format=json
+def check_ip():    
+    try:
+        response = requests.get("https://api64.ipify.org/?format=json")
+        #print(response.json()) #{"ip":"180.92.96.163"}
+        return response.json()["ip"]
+    except:
+        return None
+#'''
